ed16887f910d38d2b5df22d19dc6b951b15d7864
Sven Dobbe <dobbe.sven@hotmail.nl>
2023-12-02 06:45:26 +0100

    day 2 part 2

--- AoC2023/days/day 2/day2.cs
+++ AoC2023/days/day 2/day2.cs
@@ -23,28 +23,68 @@ public class Day2: Day
         string? line;
         while ((line = data.ReadLine()) != null)
         {
+            int linemaxred = 0;
+            int linemaxblue = 0;
+            int linemaxgreen = 0;
+            
             bool validGame = true;
             string[] content = line.Split(':');
             int gameID = int.Parse(content[0].Substring(5));
             string[] contentdata = content[1].Split(';');
             for (int i = 0; i < contentdata.Length; i++)
             {
+                int redcount = 0;
+                int bluecount = 0;
+                int greencount = 0;
+
                 var redmatches = Regex.Matches(contentdata[i], redregex);
                 var bluematches = Regex.Matches(contentdata[i], blueregex);
                 var greenmatches = Regex.Matches(contentdata[i], greenregex);
 
-                if ((redmatches.Count > 0 && redmatches[0].Groups.Count > 1 && int.Parse(redmatches[0].Groups[1].Value) > maxred) || 
-                    (bluematches.Count > 0 && bluematches[0].Groups.Count > 1 && int.Parse(bluematches[0].Groups[1].Value) > maxblue) ||
-                    (greenmatches.Count > 0 && greenmatches[0].Groups.Count > 1 && int.Parse(greenmatches[0].Groups[1].Value) > maxgreen))
+                if (redmatches.Count > 0 && redmatches[0].Groups.Count > 1)
                 {
-                    validGame = false;
+                    redcount = int.Parse(redmatches[0].Groups[1].Value);
+                    if (redcount > linemaxred)
+                    {
+                        linemaxred = redcount;
+                    }
                 }
+
+                if (bluematches.Count > 0 && bluematches[0].Groups.Count > 1)
+                {
+                    bluecount = int.Parse(bluematches[0].Groups[1].Value);
+                    if (bluecount > linemaxblue)
+                    {
+                        linemaxblue = bluecount;
+                    }
+                }
+
+                if (greenmatches.Count > 0 && greenmatches[0].Groups.Count > 1)
+                {
+                    greencount = int.Parse(greenmatches[0].Groups[1].Value);
+                    if (greencount > linemaxgreen)
+                    {
+                        linemaxgreen = greencount;
+                    }
+                }
+
             }
 
-            if (validGame)
+            int power = linemaxred * linemaxblue * linemaxgreen;
+            sum += power;
+
+            /* part 1
+
+            if (redcount > maxred || bluecount > maxblue || greencount > maxgreen)
             {
-                sum += gameID;
+                validGame = false;
             }
+        }
+
+        if (validGame)
+        {
+            sum += gameID;
+        }*/
         }
         
         Console.WriteLine(sum);
