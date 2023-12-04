a0cd9037fa81442ea28bc1183c4f8c14cc6a9b70
Sven Dobbe <dobbe.sven@hotmail.nl>
2023-12-04 13:35:41 +0100

    day 4 part 1

--- AoC2023/Program.cs
+++ AoC2023/Program.cs
@@ -1,6 +1,6 @@
-﻿using AoC2023.days.day_3;
+﻿using AoC2023.days.day_4;
 using AoC2023.tools;
 
-Day currentDay = new Day3();
+Day currentDay = new Day4();
 
 currentDay.Run();
--- AoC2023/days/day 4/day4.cs
+++ AoC2023/days/day 4/day4.cs
@@ -10,11 +10,38 @@ public class Day4: Day
     }
     public override void Run()
     {
+        int sum = 0;
         StreamReader data = LoadData();
         string? line;
         while ((line = data.ReadLine()) != null)
         {
-            Console.WriteLine(line);
+            int winningcount = 0;
+            string[] carddata = line.Split(":");
+            string[] numbers = carddata[1].Split("|");
+            List<string> winningnumbers = numbers[0].Trim().Split(" ").ToList();
+            List<string> yournumbers = numbers[1].Trim().Split(" ").ToList();
+            
+            while (winningnumbers.Contains(""))
+            {
+                winningnumbers.Remove("");
+            }
+            while (yournumbers.Contains(""))
+            {
+                yournumbers.Remove("");
+            }
+            foreach (string number in yournumbers)
+            {
+                if (winningnumbers.Contains(number))
+                {
+                    winningcount++;
+                }
+            }
+
+            if (winningcount > 0)
+            {
+                sum += (int) Math.Pow(2, winningcount - 1);
+            }
         }
+        Console.WriteLine(sum);
     }
 }
