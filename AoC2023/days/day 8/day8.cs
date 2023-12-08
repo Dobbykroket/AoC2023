7e8aff3aa70ff7b77f194dcdd1cc89f8fa204f50
Sven Dobbe <dobbe.sven@hotmail.nl>
2023-12-08 06:28:18 +0100

    day 8 part 1

--- AoC2023/days/day 8/day8.cs
+++ AoC2023/days/day 8/day8.cs
@@ -12,9 +12,47 @@ public class Day8: Day
     {
         StreamReader data = LoadData();
         string? line;
+
+        int stepcount = 0;
+
+        string instructions = data.ReadLine();
+        data.ReadLine(); // clear blank line
+        
+        Dictionary<string, string[]> nodes = new Dictionary<string, string[]>();
+
         while ((line = data.ReadLine()) != null)
         {
-            Console.WriteLine(line);
+            string[] input = line.Split("=", StringSplitOptions.TrimEntries);
+            nodes.Add(input[0], input[1].Substring(1, input[1].Length - 2).Split(",", StringSplitOptions.TrimEntries));
         }
+
+        int index = 0;
+        string currentnode = "AAA";
+
+        while (!currentnode.Equals("ZZZ"))
+        {
+            stepcount++;
+            index %= instructions.Length;
+
+            char currentinstruction = instructions[index];
+            
+            switch (currentinstruction)
+            {
+                case 'L':
+                {
+                    currentnode = nodes.GetValueOrDefault(currentnode)[0];
+                    break;
+                }
+                case 'R':
+                {
+                    currentnode = nodes.GetValueOrDefault(currentnode)[1];
+                    break;
+                }
+            }
+
+            index++;
+        }
+        
+        Console.WriteLine(stepcount);
     }
 }
