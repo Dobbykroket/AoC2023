7521272d869a0371e2753137306bf733d7c1baec
Sven Dobbe <dobbe.sven@hotmail.nl>
2023-12-09 11:32:10 +0100

    day 9 part 1

--- AoC2023/days/day 9/day9.cs
+++ AoC2023/days/day 9/day9.cs
@@ -8,13 +8,49 @@ public class Day9: Day
     {
         this.Directory = "day 9";
     }
+
+    public int[] FindDifferences(int[] nums)
+    {
+        int[] results = new int[nums.Length - 1];
+
+        for (int i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i + 1] - nums[i];
+        }
+
+        return results;
+    }
+
+    public int[] AddNextValue(int[] nums)
+    {
+        List<int> vals = new List<int>();
+        vals.AddRange(nums);
+        if (nums.All(num => num == 0))
+        {
+            vals.Add(0);
+            return vals.ToArray();
+        }
+
+        int[] diffs = AddNextValue(FindDifferences(nums));
+
+        vals.Add(vals[^1] + diffs[^1]);
+        return vals.ToArray();
+    }
+    
     public override void Run()
     {
+        int sum = 0;
+        
         StreamReader data = LoadData();
         string? line;
         while ((line = data.ReadLine()) != null)
         {
-            Console.WriteLine(line);
+            int[] nums = line.Split(" ").Select(s => int.Parse(s)).ToArray();
+            int extrapolated = AddNextValue(nums)[^1];
+
+            Console.WriteLine(extrapolated);
+            sum += extrapolated;
         }
+        Console.WriteLine(sum);
     }
 }
