8964ea9d884aca527abb1c00cb07a313abac1261
Sven Dobbe <dobbe.sven@hotmail.nl>
2023-12-13 00:33:07 +0100

    day 12 part 2

--- AoC2023/days/day 12/day12.cs
+++ AoC2023/days/day 12/day12.cs
@@ -5,24 +5,105 @@ namespace AoC2023.days.day_12;
 
 public class Day12: Day
 {
-    private bool validPlacement(string corrupteddata, int backupdata)
+    private static int recursivecalls = 0;
+    private static int memosolves = 0;
+
+    private static Dictionary<Tuple<string, int, int>, long> Possibilitymemo =
+        new Dictionary<Tuple<string, int, int>, long>();
+    private long RecursivePossibilityBuild(string corrupteddata, List<int> backupdata, int blanksquares)
     {
-        if (corrupteddata.Length < backupdata) // snippet too short to fit block
+        recursivecalls++;
+        
+        var memokey = new Tuple<string, int, int>(corrupteddata, backupdata.Count, blanksquares);
+        long shortcutvalue;
+        if (Possibilitymemo.TryGetValue(memokey, out shortcutvalue))
         {
-            return false;
+            memosolves++;
+            return shortcutvalue;
+        }
+        
+        // no more springs to add, check if any required springs left
+        if (backupdata.Count == 0) 
+        {
+            if (corrupteddata.Any(c => c.Equals('#')))
+            {
+                Possibilitymemo.Add(memokey, 0);
+                return 0;
+            }
+
+            Possibilitymemo.Add(memokey, 1);
+            return 1;
+        }
+        
+        // no more free blank spaces left, build rest of data and compare
+        if (blanksquares == 0) 
+        {
+            StringBuilder dataBuilder = new StringBuilder();
+            foreach (int length in backupdata)
+            {
+                char[] block = new char[length];
+                Array.Fill(block, '#');
+                dataBuilder.Append(block);
+                dataBuilder.Append('.');
+            }
+            dataBuilder.Length--; // remove last char from StringBuilder
+            string data = dataBuilder.ToString();
+            
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (!(corrupteddata[i].Equals('?') || corrupteddata[i].Equals(data[i])))
+                {
+                    Possibilitymemo.Add(memokey, 0);
+                    return 0;
+                }
+            }
+
+            Possibilitymemo.Add(memokey, 1);
+            return 1;
+        }
+        
+        //quickfail if there are more required springs left than we have springs left to place
+        if (backupdata.Sum() < corrupteddata.Count(c => c.Equals('#')))
+        {
+            Possibilitymemo.Add(memokey, 0);
+            return 0;
+        }
+        
+        // quickfail if we need to place more strings than we have valid placements left
+        if (backupdata.Sum() > corrupteddata.Count(c => c.Equals('#') || c.Equals('?'))) 
+        {
+            Possibilitymemo.Add(memokey, 0);
+            return 0;
         }
 
-        if (corrupteddata.Length == backupdata) // sippet exactly fits block
+        long ifempty;
+        long ifdatablock = 0;
+
+        // assume next block is a blank space, fail if required spring, otherwise recurse
+        if (corrupteddata[0].Equals('#'))
         {
-            return true;
+            ifempty = 0;
+        }
+        else
+        {
+            ifempty = RecursivePossibilityBuild(corrupteddata.Substring(1, corrupteddata.Length - 1), backupdata,
+                blanksquares - 1);
         }
 
-        if (corrupteddata[backupdata].Equals('#')) // check if this doesn't put a confirmed spot as separator
+        // assume next block is springs
+        // test spring length for required blanks
+        // check the next space after the block for a required spring, unless we're testing the end of the line
+        if (!corrupteddata.Substring(0, backupdata[0]).Any(c => c.Equals('.')) 
+            && (corrupteddata.Length == backupdata[0] || !corrupteddata[backupdata[0]].Equals('#')))
         {
-            return false;
+            List<int> shorterBackupData = backupdata.ToList();
+            shorterBackupData.Remove(shorterBackupData[0]);
+            ifdatablock = RecursivePossibilityBuild(
+                corrupteddata.Substring(backupdata[0] + 1, corrupteddata.Length - (backupdata[0] + 1)), shorterBackupData, blanksquares);
         }
 
-        return true; // no further issues
+        Possibilitymemo.Add(memokey, ifempty + ifdatablock);
+        return ifempty + ifdatablock;
     }
     
     public Day12()
@@ -31,72 +112,27 @@ public class Day12: Day
     }
     public override void Run()
     {
-        int totalpossibillities = 0;
+        long totalpossibillities = 0;
         StreamReader data = LoadData();
         string? line;
         while ((line = data.ReadLine()) != null)
         {
-            int possibillities = 0;
             string corrupteddata = line.Split(" ")[0];
-            int[] backupdata = line.Split(" ")[1].Split(",").Select(int.Parse).ToArray();
-            int blanksquares = corrupteddata.Length - (backupdata.Length - 1) - backupdata.Sum();
+            corrupteddata = string.Format("{0}?{0}?{0}?{0}?{0}", corrupteddata);
+            int[] foldedbackupdata = line.Split(" ")[1].Split(",").Select(int.Parse).ToArray();
+            List<int> backupdata = new List<int>();
+            backupdata.AddRange(foldedbackupdata);
+            backupdata.AddRange(foldedbackupdata);
+            backupdata.AddRange(foldedbackupdata);
+            backupdata.AddRange(foldedbackupdata);
+            backupdata.AddRange(foldedbackupdata);
+            int blanksquares = corrupteddata.Length - (backupdata.Count - 1) - backupdata.Sum();
 
-            int[] blocks = new int[blanksquares + backupdata.Length];
-            for (int i = 0; i < blocks.Length; i++)
-            {
-                blocks[i] = i;
-            }
-            foreach (var combination in blocks.Combinations(backupdata.Length))
-            {
-                int[] picked = new int[blocks.Length];
-                Array.Fill(picked, 0);
-
-                foreach (int pickedindex in combination)
-                {
-                    picked[pickedindex] = 1;
-                }
-
-                int backupindex = 0;
-                StringBuilder possiblelineBuilder = new StringBuilder();
-                foreach (int val in picked)
-                {
-                    if (val == 0)
-                    {
-                        possiblelineBuilder.Append(".");
-                    }
-
-                    if (val == 1)
-                    {
-                        char[] block = new char[backupdata[backupindex]];
-                        Array.Fill(block, '#');
-                        backupindex++;
-                        possiblelineBuilder.Append(block);
-                        if (backupindex != backupdata.Length)
-                        {
-                            possiblelineBuilder.Append(".");
-                        }
-                    }
-                }
-                string possibleline = possiblelineBuilder.ToString();
-                bool possible = true;
-                for (int i = 0; i < possibleline.Length; i++)
-                {
-                    if (!(corrupteddata[i].Equals('?') || corrupteddata[i].Equals(possibleline[i])))
-                    {
-                        possible = false;
-                        break;
-                    }
-                }
-                if (possible)
-                {
-                    possibillities++;
-                }
-            }
-            
-            
-
-            totalpossibillities += possibillities;
+            totalpossibillities += RecursivePossibilityBuild(corrupteddata, backupdata, blanksquares);
+            Possibilitymemo.Clear();
         }
         Console.WriteLine(totalpossibillities);
+        Console.WriteLine(recursivecalls);
+        Console.WriteLine(memosolves);
     }
 }
