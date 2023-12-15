3305288f1e0202dc851dff8ed770ad55de99c9b6
Sven Dobbe <dobbe.sven@hotmail.nl>
2023-12-15 06:48:41 +0100

    day 15 part 1

--- AoC2023/Program.cs
+++ AoC2023/Program.cs
@@ -1,6 +1,6 @@
-﻿using AoC2023.days.day_14;
+﻿using AoC2023.days.day_15;
 using AoC2023.tools;
 
-Day currentDay = new Day14();
+Day currentDay = new Day15();
 
 currentDay.TimedRun();
--- AoC2023/days/day 15/day15.cs
+++ AoC2023/days/day 15/day15.cs
@@ -1,9 +1,22 @@
-﻿using AoC2023.tools;
+﻿using System.Text;
+using AoC2023.tools;
 
 namespace AoC2023.days.day_15;
 
 public class Day15: Day
 {
+    private int Hash(String input)
+    {
+        int currentvalue = 0;
+        foreach (char c in input)
+        {
+            currentvalue += (int)c; //  dirty but works as long as input is ASCII-only
+            currentvalue *= 17;
+            currentvalue %= 256;
+        }
+
+        return currentvalue;
+    }
     public Day15()
     {
         this.Directory = "day 15";
@@ -11,10 +24,14 @@ public class Day15: Day
     public override void Run()
     {
         StreamReader data = LoadData();
-        string? line;
-        while ((line = data.ReadLine()) != null)
+        string line = data.ReadLine()!;
+        string[] steps = line.Split(",");
+        int sum = 0;
+        foreach (string step in steps)
         {
-            Console.WriteLine(line);
+            sum += Hash(step);
         }
+        
+        Console.WriteLine(sum);
     }
 }
